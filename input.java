5a77c094e2758be0bb1fee4ec44f0d1b6b87fd29
Ujwal <Walkerdoodle7@gmail.com>
2021-12-17 20:09:35 -0800

    Accepting user input and displaying using JOptionPane

---
+++ input.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+class input
+{
+	public static void main(String[] args)
+	{
+		String name= JOptionPane.showInputDialog(null,"Enter your Name : ");
+		JOptionPane.showMessageDialog(null,name);
+	}
+}
