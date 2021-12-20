61097efd52dad241baa5cbb009f8e441ea7e46f3
Ujwal <Walkerdoodle7@gmail.com>
2021-12-20 05:52:57 -0800

    updated

--- SwingSample.java
+++ SwingSample.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
-import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.FlowLayout;
 class SwingSample
 {
 	public static void main(String args[])
@@ -7,29 +9,40 @@ class SwingSample
 		guiSample trail = new guiSample();
 	}
 }
-class guiSample extends JFrame
-{	JFrame f;
+class guiSample extends JFrame implements ActionListener
+{
 	JLabel intro;
 	JLabel intro2;
 	JTextField num1;
 	JTextField num2;
 	JButton b;
+	JLabel Result=new JLabel();
 	guiSample()
 	{
-		f= new JFrame(); //creates a new objects in JFrame
-		f.setVisible(true); //making it visible
-		f.setSize(400,500); //set the size
-		f.setLayout(new FlowLayout()); //changes the cardlayout into FlowLayout
+		setVisible(true); //making it visible
+		setSize(800,400); //set the size
+		setLayout(new FlowLayout()); //changes the cardlayout into FlowLayout
 		intro = new JLabel ("Enter the first number : ");
-		num1 = new JTextField(10); //creates a textfield
+		num1 = new JTextField(5); //creates a textfield
 		intro2=new JLabel("Enter the Second number : ");
-		num2 = new JTextField(10);
+		num2 = new JTextField(5);
 		b = new JButton("Enter");
-		f.add(intro);
-		f.add(num1);
-		f.add(intro2);
-		f.add(num2);
-		f.add(b);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exits the program while closing the window
+		add(intro);
+		add(num1);
+		add(intro2);
+		add(num2);
+		add(b);
+		b.addActionListener(this);
+		setVisible(true); //making it visible
 	}
+	public void actionPerformed(ActionEvent ae)
+	{
+		int a,b,sum;
+		a=Integer.parseInt(num1.getText());
+		b=Integer.parseInt(num2.getText());
+		sum=a+b;
+		Result.setText("The sum is "+sum);
+	}
+	
 }
+
