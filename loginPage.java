86405d0ffa794c18ad3bf7d0b8f539c445ad8bab
Ujwal <Walkerdoodle7@gmail.com>
2021-12-20 06:26:44 -0800

    LoginPage Updated

--- loginPage.java
+++ loginPage.java
@@ -1,36 +1,60 @@
+import java.awt.*;
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+class LoginFrame extends JFrame implements ActionListener
+{
+	Container container = getContentPane();
+	JLabel userLabel=new JLabel("Username : ");
+	JLabel passwordLabel = new JLabel("Password : ");
+	JTextField user = new JTextField();
+	JPasswordField password = new JPasswordField();
+	JButton loginButton = new JButton("Login");
+	JButton resetButton = new JButton("Reset");
+	
+	LoginFrame()
+	{
+		setLayoutManager();
+		setLocationAndSize();
+		addComponents();
+	}
+	public void setLayoutManager()
+	{
+		container.setLayout(null);
+	}
+	public void setLocationAndSize()
+	{
+		userLabel.setBounds(50,150,100,30);
+		passwordLabel.setBounds(50,220,100,30);
+		user.setBounds(150,150,150,30);
+		password.setBounds(150,220,150,30);
+		loginButton.setBounds(50,300,100,30);
+		resetButton.setBounds(200,300,100,30);
+	}
+	public void addComponents()
+	{
+		container.add(userLabel);
+		container.add(passwordLabel);
+		container.add(user);
+		container.add(password);
+		container.add(loginButton);
+		container.add(resetButton);
+	}
+	@Override
+	public void actionPerformed(ActionEvent e)
+	{
+	}
+}
 public class loginPage
 {
-	public static void main(String args[])
+	public static void main(String[] args)
 	{
-		JPanel panel = new JPanel();
-		JFrame window = new JFrame();
-		window.setSize(200,200);
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.add(panel);
-		
-		panel.setLayout(null);
-		window.setVisibe(true);
-		JLabel user = new JLabel ("Username ");
-		user.setBounds(15,30,90,25);
-		JTextField user_in = new JTextField(20);
-		user.setBounds(50,30,180,30);
-		JLabel password = new JLabel("Password ");
-		password.setBounds(15,60,90,25);
-		JPasswordField password_in = new JPasswordField();
-		password_in.setBounds(50,60,180,30);
-		
-		JButton button = new JButton("Login");
-		button.setBounds(30,80,60,30);
-		
-
-		
-		panel.add(user);
-		panel.add(user_in);
-		panel.add(password);
-		panel.add(password_in);
-		panel.add(button);
-		
-		
+		LoginFrame frame=new LoginFrame();
+		frame.setTitle("Login Panel");
+		frame.setVisible(true);
+		frame.setBounds(10,10,370,600);
+		frame.setDefaultCloseOperation(3);
+		frame.setResizable(false);
 	}
 }
